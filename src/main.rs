22f77e5e0eb469df7b031710f7cfdb10720cf6ae
Akaiko <58089998+Akaiko1@users.noreply.github.com>
2023-12-24 22:03:04 +0400

    Refactor
    Added constants

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,40 @@
 use nannou::prelude::*;
 use rand::Rng;
 
-static AMOUNT: i16 = 5;
+const AMOUNT: i16 = 5;
+const MAX_RADIUS: f32 = 200.;
+const RADIUS_INCREMENT: f32 = 1.;
+const INITIAL_RADIUS: f32 = 10.;
+const COLOR_CHANGE_FACTOR: i16 = 7;
+const COLORS: [rgb::Srgb<u8>; 5] = [NAVAJOWHITE, LIGHTGOLDENRODYELLOW, ANTIQUEWHITE, BLANCHEDALMOND, WHITE];
 
 #[derive(Clone)]
 struct Ripple {
     center: Point2,
-    copy_color: Vec<rgb::Srgb<u8>>,
+    color_sequence: Vec<rgb::Srgb<u8>>,
     radius: f32,
-    copies: i16
+    copies: i16,
 }
 
 struct Model {
-    ripples: Vec<Ripple>
+    ripples: Vec<Ripple>,
 }
 
 impl Ripple {
-    fn grow(&mut self) -> &mut Self {
-        self.radius += 1.;
-        self
+    fn grow(&mut self) {
+        self.radius += RADIUS_INCREMENT;
     }
 
     fn copy(&mut self) {
-        if self.copies < AMOUNT && self.radius as i16 > self.copies * 7  {
+        if self.copies < AMOUNT && self.radius as i16 > self.copies * COLOR_CHANGE_FACTOR {
             self.copies += 1;
-            self.copy_color.push(get_color())
+            self.color_sequence.push(random_color());
         }
     }
 }
 
-fn get_color() -> rgb::Srgb<u8> {
-    vec![NAVAJOWHITE, LIGHTGOLDENRODYELLOW, ANTIQUEWHITE, BLANCHEDALMOND, WHITE]
-                [rand::thread_rng().gen_range(0..5)]
+fn random_color() -> rgb::Srgb<u8> {
+    COLORS[rand::thread_rng().gen_range(0..COLORS.len())]
 }
 
 fn main() {
@@ -42,49 +45,44 @@ fn main() {
 }
 
 fn model(_app: &App) -> Model {
-    Model {
-        ripples: vec![]
-    }
+    Model { ripples: vec![] }
 }
 
 fn event(app: &App, model: &mut Model, event: Event) {
-
     match event {
         Event::Update(_) => {
             for ripple in model.ripples.iter_mut() {
-                ripple.grow().copy();
+                ripple.grow();
+                ripple.copy();
             }
-
-            model.ripples.retain(|x| x.radius < 200.)
-        }
-
-        Event::WindowEvent{id: _id, simple: Some(MousePressed(_))} => {
+            model.ripples.retain(|ripple| ripple.radius < MAX_RADIUS);
+        },
+        Event::WindowEvent { simple: Some(MousePressed(_)), .. } => {
             model.ripples.push(Ripple {
                 center: app.mouse.position(),
-                copy_color: vec![get_color()],
-                radius: 10.,
-                copies: 1
+                color_sequence: vec![random_color()],
+                radius: INITIAL_RADIUS,
+                copies: 1,
             });
-        }
+        },
         _ => {}
     }
-
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(BLUE);
 
-    for ripple in model.ripples.iter() {
+    for ripple in &model.ripples {
         for i in 0..ripple.copies {
             draw.ellipse()
-            .xy(ripple.center)
-            .w_h(ripple.radius - i as f32 * 7., ripple.radius - i as f32 * 7.)
-            .no_fill()
-            .stroke(ripple.copy_color[i as usize])
-            .stroke_weight(3.0);
+                .xy(ripple.center)
+                .w_h(ripple.radius - i as f32 * COLOR_CHANGE_FACTOR as f32, ripple.radius - i as f32 * COLOR_CHANGE_FACTOR as f32)
+                .no_fill()
+                .stroke(ripple.color_sequence[i as usize])
+                .stroke_weight(3.0);
         }
     }
-    
+
     draw.to_frame(app, &frame).unwrap();
 }
